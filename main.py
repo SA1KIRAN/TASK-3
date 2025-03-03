64a9de74a48fb03975866b75c452c6b19b585cd3
SA1KIRAN <rsk7069@gmail.com>
2025-03-03 07:41:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -27,15 +27,17 @@ import random
 words = ['python', 'javascript', 'java', 'automation', 'pytest', 'guvi', 'selenium']
 
 selection = random.choice(words)
-
 scrambled_word=''.join(random.sample(selection, len(selection)))
 
 print(scrambled_word)
 
-answer=input("answer the original word")
+attempts = 0
 
-if (answer.lower()==selection.lower()):
-    print("right answer")
-else:
-    print("wrong answer correct answer is ", selection)
+while True:
+    answer=input("answer the original word")
+    attempts +=1
+    if (answer.lower()==selection.lower()):
+        print("right answer")
+    else:
+        print("wrong answer try again")
 
